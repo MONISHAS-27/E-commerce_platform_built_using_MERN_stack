ef890080e80fccd083213ab7f623819f2fe4ee6b
Monisha <129679670+MONISHAS-27@users.noreply.github.com>
2025-03-17 11:50:28 +0530

    Update NewCollections.jsx with backend url

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -7,7 +7,7 @@ const NewCollections = () => {
   const [new_collection, setNew_Collection] = useState([]);
 
   useEffect(() =>{
-    fetch('http://localhost:4000/newcollections')
+    fetch('https://e-commerce-platform-built-using-mern.onrender.com/newcollections')
     .then((response) =>response.json())
     .then((data)=>setNew_Collection(data));
   },[])
